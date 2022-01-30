c5b741b4fb9ac408d61d45e0976445d65e250c74
jolsten <jolsten@gmail.com>
2022-01-30 09:56:32 -0500

    renamed to typeconverter

--- src/typeconverter/twoscomp.py
+++ src/typeconverter/twoscomp.py
@@ -5,4 +5,4 @@ from .types import UnsignedInteger, SignedInteger
 def uint_to_twoscomp(value: UnsignedInteger, size: np.uint8) -> SignedInteger:
     if (value & (1 << (size - 1))) != 0: # if sign bit is set e.g., 8bit: 128-255
         value = value - (1 << size)      # compute negative value
-    return val                           # return positive value as is
+    return value                         # return positive value as is

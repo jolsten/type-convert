a8a9e2bd24a90ea4e55672459268308e3993c750
jolsten <jolsten@gmail.com>
2022-01-31 20:19:58 -0500

    cleaned up ones and twos complement converters

--- src/typeconverter/onescomp.py
+++ src/typeconverter/onescomp.py
@@ -17,39 +17,5 @@ def func(value: UnsignedInteger, size: np.uint8) -> SignedInteger:
         return -np.int64((~(value << pad_bits)) >> pad_bits)
     return value
 
-    # if size == 8:
-    #     value = np.uint8(value).view(np.int8)
-    #     if value < 0:
-    #         value += np.int8(1)
-    #     return value
-    # elif size == 16:
-    #     value = np.uint16(value).view(np.int16)
-    #     if value < 0:
-    #         value += np.int16(1)
-    #     return value
-    # elif size == 32:
-    #     value = np.uint32(value).view(np.int32)
-    #     if value < 0:
-    #         value += np.int32(1)
-    #     return value
-    # elif size == 64:
-    #     value = np.uint64(value).view(np.int64)
-    #     if value < 0:
-    #         value += np.int64(1)
-    #     return value
-
-    # value = np.uint64(value)
-    # if value >= 2**(size-1):
-    #     pad_bits = np.uint8(64 - size)
-    #     print('x', value, type(value))
-    #     value = np.int64(value << pad_bits) >> pad_bits
-    #     print('y', value, type(value))
-    #     value += np.int64(1)
-    #     #value = (np.uint64(value) << np.uint8(64 - size)).view(np.int64) >> np.uint8(64-size)) + np.int64(1)
-    # return value
-    # if (value & (1 << (size - 1))) != 0: # if sign bit is set e.g., 8bit: 128-255
-    #     value = value - (1 << size) + 1  # compute negative value, add one
-    # return value                         # return positive value as is
-
 jfunc = njit(signatures)(func)
 ufunc = vectorize(signatures)(func)
--- src/typeconverter/utils.py
+++ src/typeconverter/utils.py
@@ -5,3 +5,9 @@ def bits_to_wordsize(size: np.uint8) -> np.uint8:
         if size <= word_size:
             return word_size
     return 64
+
+def mask(size: int) -> int:
+    mask = 1
+    for _ in range(size-1):
+        mask = (mask << 1) + 1
+    return mask

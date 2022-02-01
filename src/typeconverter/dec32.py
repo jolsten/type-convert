f2e173389a83f0a0402b4c8f4b40a403c00d803a
jolsten <jolsten@gmail.com>
2022-01-31 22:09:49 -0500

    added dec32

---
+++ src/typeconverter/dec32.py
@@ -0,0 +1,24 @@
+import numpy as np
+from numba import njit, vectorize
+
+from .types import UnsignedInteger, SignedInteger
+
+signatures = [
+    'f8(u4)',
+]
+
+def func(value: np.uint32) -> np.float64:
+    value = np.uint32(value)
+
+    s = (value & np.uint32(0x80000000)) >> np.uint8(31)
+    e = (value & np.uint32(0x7F800000)) >> np.uint8(23)
+    m = (value & np.uint32(0x007FFFFF))
+
+    S = np.int8(-1) ** s
+    E = np.int16(e) - np.int16(128)
+    M = np.float64(m) / np.float64(2**24)
+    
+    return np.float64(S * M * np.float64(2)**E)
+
+jfunc = njit(signatures)(func)
+ufunc = vectorize(signatures)(func)
---
+++ tests/test_dec32.py
@@ -0,0 +1,41 @@
+import pytest
+import numpy as np
+from numba import njit, vectorize
+from itertools import zip_longest
+
+from typeconverter.utils import bits_to_wordsize, mask
+from typeconverter.dec32 import func, jfunc, ufunc
+
+TEST_ARRAY_SIZE = 100
+
+S = (0b0, 0b1)
+E = (0b00000000, 0b11111111)
+M = (0x00000000, 0x007FFFFF)
+
+tests = []
+for s in S:
+    for e in E:
+        for m in M:
+            val_in = np.uint32( (s<<31) + (e<<23) + m )
+            val_out = (-1)**s * (m / 2**24) * 2**(e - 128)
+            tests.append( (val_in, pytest.approx(val_out)) )
+
+
+@pytest.mark.parametrize('val_in, val_out', tests)
+def test_func(val_in, val_out):
+    print('func')
+    assert func(val_in) == val_out
+
+
+@pytest.mark.parametrize('val_in, val_out', tests)
+def test_njit(val_in, val_out):
+    print('jfunc', val_in, val_out)
+    assert jfunc(val_in) == val_out
+
+
+@pytest.mark.parametrize('val_in, val_out', tests)
+def test_vectorize(val_in, val_out):
+    print('ufunc')
+    data = np.array([val_in] * TEST_ARRAY_SIZE)
+    ufunc(data)
+    assert all([a==b for a, b in zip_longest(ufunc(data), [val_out] * TEST_ARRAY_SIZE)])

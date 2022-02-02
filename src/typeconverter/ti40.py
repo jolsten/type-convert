81ee3c4c4e4275e84ae2a577f12245504ce1974c
jolsten <jolsten@gmail.com>
2022-02-01 21:49:42 -0500

    removed erroneous print statements from testing

--- src/typeconverter/ti40.py
+++ src/typeconverter/ti40.py
@@ -10,15 +10,22 @@ signatures = [
 from typeconverter.twoscomp import jfunc as uint_to_twoscomp
 
 def func(value: np.uint64) -> np.float64:
+    # Reference:
+    # Telemetry Standards, RCC Standard 106-20 Chapter 9, July 2020
     value = np.uint64(value)
-    
-    e = uint_to_twoscomp((value & np.uint64(0xFF00000000)) >> np.uint8(32), np.uint8( 8))
-    m = uint_to_twoscomp((value & np.uint64(0x00FFFFFFFF))                , np.uint8(32))
 
+    e = uint_to_twoscomp((value >> np.uint8(32)) & np.uint64(0xFF), np.uint8(8))
+    s = (value >> np.uint8(31)) & np.uint64(1)
+    m = (value & np.uint64(0x007FFFFFFF))
+
+    if e == np.int64(-128):
+        return np.float64(0)
+
+    S = np.float64(-2) ** s
     E = np.float64(e)
-    M = np.float64(m) / np.float64(2**31)
+    M = np.float64(m)
 
-    return M * np.float64(2) ** E
+    return ( S + M/np.float64(2**31) ) * np.float64(2) ** E
 
 jfunc = njit(signatures)(func)
 ufunc = vectorize(signatures)(func)

a26b411f19e584982d9f2696af1a9275806b2873
jolsten <jolsten@gmail.com>
2022-02-01 21:59:32 -0500

    moved types to sub-package, added njit and ufunc for easy grabbing of relevant functions

---
+++ src/typeconverter/njit.py
@@ -0,0 +1,13 @@
+import typeconverter.types as tct
+
+from tct.onescomp import njit as onescomp
+from tct.twoscomp import njit as twoscomp
+
+from tct.milstd1750a32 import njit as milstd1750a32
+from tct.milstd1750a48 import njit as milstd1750a48
+
+from tct.ti32 import njit as ti32
+from tct.ti40 import njit as ti40
+
+from tct.dec32 import njit as dec32
+from tct.dec64 import njit as dec64
---
+++ src/typeconverter/ufunc.py
@@ -0,0 +1,13 @@
+import typeconverter.types as tct
+
+from tct.onescomp import ufunc as onescomp
+from tct.twoscomp import ufunc as twoscomp
+
+from tct.milstd1750a32 import ufunc as milstd1750a32
+from tct.milstd1750a48 import ufunc as milstd1750a48
+
+from tct.ti32 import ufunc as ti32
+from tct.ti40 import ufunc as ti40
+
+from tct.dec32 import ufunc as dec32
+from tct.dec64 import ufunc as dec64

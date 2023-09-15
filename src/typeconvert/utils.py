46cc01e6df3c18f4456d666f2663280b21e208ff
jolsten <jolsten@gmail.com>
2023-09-14 22:27:46 -0400

    trying to resolve pytest issues

--- src/typeconvert/utils.py
+++ src/typeconvert/utils.py
@@ -3,7 +3,7 @@ from typing import Optional
 import numpy as np
 from numpy.typing import DTypeLike
 
-NPY_CAST_SAFE = pp.parse(np.__version__) >= pp.parse("1.24")
+NPY_CAST_SAFE = pp.parse(np.__version__) >= pp.parse("1.23")
 
 
 def bits_to_wordsize(size: np.uint8) -> np.uint8:

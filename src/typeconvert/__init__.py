15685575d85e212ebe52a845c4e77220d2c3cfb8
Jonathan Olsten <jolsten@gmail.com>
2023-09-15 17:21:36 -0400

    fixed version() call in __init__

--- src/typeconvert/__init__.py
+++ src/typeconvert/__init__.py
@@ -4,7 +4,7 @@ except ImportError:
     from importlib.metadata import version, PackageNotFoundError
 
     try:
-        __version__ = version("package-name")
+        __version__ = version("typeconvert")
     except PackageNotFoundError:
         # package is not installed
         pass

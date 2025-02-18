df5d0bd9980150d173062448f13286b3647d63f1
Jonathan Olsten <jolsten@gmail.com>
2025-02-18 06:19:49 -0500

    Upgrade to numpy>=2, build with uv (#2)
    * Converted the project to use "uv"
    * Upgraded to numpy >= 2.0

--- setup.py
+++ setup.py
@@ -1,12 +1,33 @@
-import numpy as np
+import pathlib
+
+import numpy
 from setuptools import Extension, setup
+from setuptools_scm.version import get_local_dirty_tag
+
+
+def clean_scheme(version):
+    return get_local_dirty_tag(version) if version.dirty else "+clean"
+
+
+npy_include = numpy.get_include()
+typeconvert_src = pathlib.Path(__file__).parent / "src" / "typeconvert" / "_c"
+includes = [npy_include, typeconvert_src]
 
 extensions = [
-    Extension("func", ["src/typeconvert/_c/func.c"], include_dirs=[np.get_include()]),
-    Extension("ufunc", ["src/typeconvert/_c/ufunc.c"], include_dirs=[np.get_include()]),
+    Extension(
+        "typeconvert._c.func",
+        sources=["src/typeconvert/_c/func.c"],
+        include_dirs=includes,
+    ),
+    Extension(
+        "typeconvert._c.ufunc",
+        sources=["src/typeconvert/_c/ufunc.c"],
+        include_dirs=includes,
+    ),
 ]
 
 setup(
-    ext_package="typeconvert_ext",
+    name="typeconvert",
+    use_scm_version={"local_scheme": clean_scheme},
     ext_modules=extensions,
 )

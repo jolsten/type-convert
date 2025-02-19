1a1d5aa01e841be9382af366a9711721f4747c05
Jonathan Olsten <jolsten@gmail.com>
2025-02-18 20:28:01 -0500

    fixing setuptools_scm +clean suffix

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools_scm.version import get_local_dirty_tag
 
 
 def clean_scheme(version):
-    return get_local_dirty_tag(version) if version.dirty else "+clean"
+    return get_local_dirty_tag(version) if version.dirty else ""
 
 
 npy_include = numpy.get_include()
